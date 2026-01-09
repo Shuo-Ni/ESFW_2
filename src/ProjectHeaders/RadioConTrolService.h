34fae96ae5d6d70b22fa3357d06449d531fd078a
Shuo-Ni <wenshuo@toughbull.com>
2026-01-09 16:18:38 +0800

    Update RadioConTrolService.h

--- src/ProjectHeaders/RadioConTrolService.h
+++ src/ProjectHeaders/RadioConTrolService.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include "../FrameworkHeaders/ES_Events.h"
 
-// 公开函数原型
+// Public function prototype
 bool InitRadioControlService(uint8_t Priority);
 bool PostRadioControlService(ES_Event_t ThisEvent);
 ES_Event_t RunRadioControlService(ES_Event_t ThisEvent);
